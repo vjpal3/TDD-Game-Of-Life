3fbcd164f0a10311a2fc577f322e5ebdb488471d
vjpal3 <vrishalipal@yahoo.com>
2020-01-16 23:22:25 -0500

    Refactor code

--- Conways.Library/LifeRules.cs
+++ Conways.Library/LifeRules.cs
@@ -14,16 +14,29 @@ namespace Conways.Library
         // The approach here is functional programming rather than OOP
         public static CellState GetNewState(CellState currentState, int liveNeighbors)
         {
-            if (currentState == CellState.Alive && liveNeighbors < 2)
-                return CellState.Dead;
-            
-            if (currentState == CellState.Alive && liveNeighbors > 3)
-                return CellState.Dead;
-            
-            if (currentState == CellState.Dead && liveNeighbors == 3)
-                return CellState.Alive;
-
+            switch(currentState)
+            {
+                case CellState.Alive:
+                    if(liveNeighbors < 2 || liveNeighbors > 3)
+                        return CellState.Dead;
+                    break;
+                case CellState.Dead:
+                    if(liveNeighbors == 3)
+                        return CellState.Alive;
+                    break;
+            }
             return currentState;
+
+            //if (currentState == CellState.Alive && liveNeighbors < 2)
+
+
+            //if (currentState == CellState.Alive && liveNeighbors > 3)
+            //    return CellState.Dead;
+
+            //if (currentState == CellState.Dead && liveNeighbors == 3)
+            //    return CellState.Alive;
+
+            //return currentState;
 
         }
     }
